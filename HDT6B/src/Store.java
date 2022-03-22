c39dcf26e68de0127ad7583759cba7d05dd72156
gusanitor8 <gusanitor8@gmail.com>
2022-03-21 23:21:05 -0600

    Semi final

--- HDT6B/src/Store.java
+++ HDT6B/src/Store.java
@@ -7,7 +7,10 @@ import java.util.Map;
 
 public class Store {
 	Vista vista = new Vista();
-	Map<String, String> map;
+	Map<String, String[]> map;
+	String[] products; //keys
+	boolean toRun = true;
+
 	
 	public void run() {
 		this.factory();			
@@ -16,22 +19,67 @@ public class Store {
 
 	}
 	
+	public Store() {
+		switch(vista.askImplementation()) {
+		case 1:
+			map = new HashMap<String, String[]>();
+			break;
+		case 2:
+			map = new TreeMap<String, String[]>();
+			break;
+		case 3:
+			map = new LinkedHashMap<String, String[]>();
+			break;
+		}
+	}
+	
 	
 	/**
 	 * Metodo que implementa el patron de disenio Factory
 	 * para las clases que implementan la interfaz mapa	
 	 */
-	public void factory() {		 				
-		switch(vista.askImplementation()) {
-			case 1:
-				map = new HashMap<String, String>();
-				break;
-			case 2:
-				map = new TreeMap<String, String>();
-				break;
-			case 3:
-				map = new LinkedHashMap<String, String>();
-				break;
+	public void factory() {	
+		String product;						
+		toMap(vista.toRead());
+		
+		while (toRun) {
+			long start;
+			long end;
+			switch(vista.menuOpiones()) {
+				case 1:
+					
+					product = vista.askProduct(products);
+					addProductToCollection(product);
+					break;
+				case 2:
+					product = vista.askProduct(products);
+				    getCategory(product);
+					break;
+				case 3:
+					
+					product = vista.askProduct(products);
+					start = System.currentTimeMillis();
+					productInfo(product);
+					end = System.currentTimeMillis();
+					
+					System.out.println(end-start);
+					break;
+				case 4:
+					start = System.currentTimeMillis();
+					collectionInfo();
+					end = System.currentTimeMillis();
+					System.out.println(end-start);
+					break;
+				case 5:
+					start = System.currentTimeMillis();
+					showInventory();
+					end = System.currentTimeMillis();
+					System.out.println(end-start);
+					break;
+				case 6:
+					toRun = false;
+					break;
+			}
 		}
 	}
 	
@@ -45,14 +93,80 @@ public class Store {
 			valueKey[0] = valueKey[0].strip();
 			valueKey[1] = valueKey[1].strip();
 			
-			//String[] valueArray = {valueKey[0]};
-				
-			System.out.println("Llave: " + valueKey[1] + " Valor: " + valueKey[0]);
+			String[] valueArray = {valueKey[0], "0"};							
+			//System.out.println("Llave: " + valueKey[1] + " Valor: " + valueKey[0]); //BORRAR LINEA
 			
-			map.put(valueKey[1], valueKey[0]);
+			map.put(valueKey[1], valueArray);
+			products = map.keySet().toArray(new String[map.size()]); //array de las llaves (productos)
 						
 		}
 		
 		
+	}
+	
+	/**
+	 * Agrega el producto ingresado por el usuario a su coleccion
+	 * @param product producto ingresado por el usuario
+	 */
+	public void addProductToCollection(String product) {
+		String[] valueArray = map.get(product);
+		int quantity = Integer.parseInt(valueArray[1]) + 1; 
+		//System.out.println("cantidad: "+ quantity);			//Borrar linea
+		String cantidad = String.valueOf(quantity);
+		valueArray[1] = cantidad;
+		
+		map.put(product, valueArray);
+		
+		vista.productAddedFeedback(product, quantity);
+	}
+	
+	
+	public void getCategory(String product) {
+		String category = map.get(product)[0];
+		vista.categoryFeedback(product, category);
+	}
+	
+	
+	public void collectionInfo() {
+		for (String key: products) {
+			String[] valueArray = map.get(key);
+			
+			if(valueArray[1].equals("0") == false) {
+				String cantidad = valueArray[1];
+				String categoria = valueArray[0];
+				
+				vista.productInfoFeedback(key, cantidad, categoria);
+			}
+			
+		}
+	}
+	
+	public void productInfo(String product) {
+		boolean productFound = false;
+		
+		for (String key: products) {
+			String[] valueArray = map.get(key);
+			
+			if(key.equals(product)) {
+				String cantidad = valueArray[1];
+				String categoria = valueArray[0];
+				
+				vista.productInfoFeedback(key, cantidad, categoria);
+			}			
+		}
+		
+		if (productFound == false) {
+			vista.productNotFound();
+		}
+	}
+	
+	public void showInventory() {
+		for (String key: products) {
+			String[] valueArray = map.get(key);
+			String cantidad = valueArray[1];
+			String categoria = valueArray[0];	
+			
+			vista.productInfoFeedback(key, cantidad, categoria);
+		}
 	}
 }
--- HDT6B/src/Vista.java
+++ HDT6B/src/Vista.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -71,6 +73,108 @@ public class Vista {
 		
 		return strings;
 	}
+	
+	/**
+	 * Le pregunta al ususario por algun producto en la coleccion
+	 * @param listaProductos lista de todos los productos en la coleccion
+	 * @return devuelve el producto que el usuarioo ingreso si es que existe 
+	 */
+	public String askProduct(String[] listaProductos) {
+		String producto = "";
+		
+		try {
+			print("Ingrese el producto: ");
+			producto = input.nextLine();			
+			
+			
+			if (isInArray(listaProductos, producto) == false) {
+				throw new IndexOutOfBoundsException();
+			}
+			
+			
+		}catch(IndexOutOfBoundsException e) {
+			print("Ese producto no se encuentra en la coleccion");
+		}
+		
+		return producto;
+				
+	}
+	
+	/**
+	 * Verifica si el elemento esta en la lista
+	 * @param array lista a verificar
+	 * @param element elemento a encontrar
+	 * @return booleano, devuelve true si el elemento esta en el array
+	 */
+	public boolean isInArray(String[] array, String element) {
+		boolean isInArray = false;
+		
+		for (String value : array) {
+			if(value.equals(element)) {
+				isInArray = true;
+			}
+		}
+		
+		
+		return isInArray;
+	}
+	
+	
+	/**
+	 * Menu de opciones
+	 * @return devuelve un numero entero de 1 a 5 (inclusivo)
+	 */
+	public int menuOpiones() {
+		print("Bienvenido a la tienda");
+		print("1. Agregar producto a mi coleccion");
+		print("2. Mostrar categoria de un producto");
+		print("3. Mostrar datos de un producto");
+		print("4. Mostrar categoria y cantidad de productos en mi coleccion");
+		print("5. Mostrar producto y categoria del inventario");
+		print("6. Salir");
+		print("Ingrese una opcion: ");
+		
+		
+		int option = 0;
+		try {
+			option = input.nextInt();
+			input.nextLine();
+					
+			if ((option < 1) || (option > 6)) {
+				throw new IndexOutOfBoundsException();
+			}
+			
+		}catch(IndexOutOfBoundsException e) {
+			print("La opcion esta fuera de las opciones, intente de nuevo\n");
+			option = menuOpiones();
+			
+		}catch(InputMismatchException e) {
+			print("La opcion ingresada es incorrecta, intente de nuevo\n");
+			input.nextLine();
+			option = menuOpiones();
+			
+		}
+		return option;
+	}
+	
+	
+	public void productAddedFeedback(String product, int quant) {
+		print("Se agrego: " + product + " a su coleccion, y ahora cuenta con: " + quant + " de ese producto e su coleccion");		
+	}
+	
+	public void categoryFeedback(String product, String cat) {
+		print("El producto: " + product + " entra en la categoria: " + cat);
+	}
+	
+	public void productInfoFeedback(String product, String can, String category) {
+		print("El producto: " + product + " pertenece a la categoria: " +category + " y cuenta con: " + can + " elementos en su coleccion");
+		
+	}
+	
+	public void productNotFound() {
+		print("El producto que ingreso no existe, intente escribirlo EXACTAMENTE como esta indicado en el txt \n");
+	}
+	
 	
 	
 }
